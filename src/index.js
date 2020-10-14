f7113d6d6e83291059d0ccafd6296be4e7ae0aaa
Shahid Rahim <shahid@cobold.in>
2020-10-14 13:33:37 +0530

    SHA: added reducer and store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+import store from './store'
+
+
+/*
+  This subscribe() method return a function for unsubscribing from the store
+*/
+const unsubscribe = store.subscribe(()=>{
+  console.log('Store changes:', store.getState())
+})
+
+store.dispatch({
+  type: 'bugAdded',
+  payload: {
+    description: 'Bug1'
+  }
+})
+
+unsubscribe()
+
+store.dispatch({
+  type: 'bugRemoved',
+  payload: {
+    id: 1
+  }
+})
+
+console.log(store.getState())
